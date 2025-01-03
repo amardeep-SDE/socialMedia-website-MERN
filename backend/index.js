4cb7da1b4bade8f7716cb7e91a9c192c6a494b35
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-01-04 01:54:39 +0530

    feat: message and post controller, message and post route fix: user controller and index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
 import userRoute from './routes/user.route.js';
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -24,7 +26,10 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// yha pr apni api ayengi
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
 
 
 app.listen(PORT, () => {
