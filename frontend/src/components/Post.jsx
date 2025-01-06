a2396e65fd083d5e3ed8f20bc602ddebe95e4dbe
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-01-06 22:11:37 +0530

    feat: post fix: some ui changes

---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,106 @@
+import { Bookmark, MessageCircle, MoreHorizontal, Send } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useState } from "react";
+import CommentDialog from "./CommentDialog";
+
+const Post = () => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const changeEventHandler = (e) => {
+    const inputText = e.target.value;
+    if (inputText.trim()) {
+      setText(inputText);
+    } else {
+      setText("");
+    }
+  };
+
+  return (
+    <div className="my-8 w-full max-w-sm mx-auto">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage src="" alt="post_image" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="flex items-center gap-3">
+            <h1>amar</h1>
+          </div>
+        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <MoreHorizontal className="cursor-pointer" />
+          </DialogTrigger>
+          <DialogContent className="flex flex-col items-center text-sm text-center">
+            <Button
+              variant="ghost"
+              className="cursor-pointer w-fit text-[#ED4956] font-bold"
+            >
+              Unfollow
+            </Button>
+
+            <Button variant="ghost" className="cursor-pointer w-fit">
+              Add to favorites
+            </Button>
+
+            <Button variant="ghost" className="cursor-pointer w-fit">
+              Delete
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <img
+        className="rounded-sm my-2 w-full aspect-square object-cover"
+        src="https://plus.unsplash.com/premium_photo-1683140655656-20448abc55da?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww"
+        alt="post_img"
+      />
+      <div className="flex items-center justify-between my-2">
+        <div className="flex items-center gap-3">
+          <FaHeart size={"24"} className="cursor-pointer text-red-600" /> :{" "}
+          <FaRegHeart
+            size={"22px"}
+            className="cursor-pointer hover:text-gray-600"
+          />
+          <MessageCircle
+            onClick={() => {
+              setOpen(true);
+            }}
+            className="cursor-pointer hover:text-gray-600"
+          />
+          <Send className="cursor-pointer hover:text-gray-600" />
+        </div>
+        <Bookmark className="cursor-pointer hover:text-gray-600" />
+      </div>
+      <span className="font-medium block mb-2">1 likes</span>
+      <p>
+        <span className="font-medium mr-2">amar</span>
+        amar
+      </p>
+      <span
+        onClick={() => {
+          setOpen(true);
+        }}
+        className="cursor-pointer text-sm text-gray-400"
+      >
+        View all 2 comments
+      </span>
+      <CommentDialog open={open} setOpen={setOpen} />
+      <div className="flex items-center justify-between">
+        <input
+          type="text"
+          placeholder="Add a comment..."
+          value={text}
+          onChange={changeEventHandler}
+          className="outline-none text-sm w-full"
+        />
+        {text && <span className="text-[#3BADF8] cursor-pointer">Post</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Post;
