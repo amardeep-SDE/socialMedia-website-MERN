831a8ae2af55140d4bbd351432c4195972977a27
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-01-26 23:50:36 +0530

    fix: implement socket in app.js and chatPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useDispatch, useSelector } from "react-redux";
 import ChatPage from "./components/ChatPage";
 import EditProfile from "./components/EditProfile";
 import Home from "./components/Home";
@@ -6,6 +7,11 @@ import MainLayout from "./components/MainLayout";
 import Profile from "./components/Profile";
 import Signup from "./components/Signup";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
+import { setOnlineUsers } from "./redux/chatSlice";
+import { setLikeNotification } from "./redux/rtnSlice";
+import { setSocket } from "./redux/socketSlice";
 
 const browserRouter = createBrowserRouter([
   {
@@ -41,6 +47,40 @@ const browserRouter = createBrowserRouter([
 ]);
 
 function App() {
+
+  const { user } = useSelector(store => store.auth);
+  const { socket } = useSelector(store => store.socketio);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) {
+      const socketio = io('http://localhost:8000', {
+        query: {
+          userId: user?._id
+        },
+        transports: ['websocket']
+      });
+      dispatch(setSocket(socketio));
+
+      // listen all the events
+      socketio.on('getOnlineUsers', (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+
+      socketio.on('notification', (notification) => {
+        dispatch(setLikeNotification(notification));
+      });
+
+      return () => {
+        socketio.close();
+        dispatch(setSocket(null));
+      }
+    } else if (socket) {
+      socket.close();
+      dispatch(setSocket(null));
+    }
+  }, [user, dispatch]);
+
   return (
     <>
       <RouterProvider router={browserRouter} />
