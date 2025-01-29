f0f22e91ff47b5a0225a852802bfdb55ea0ee11f
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-01-29 23:30:44 +0530

    fix : messageController and message model

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -7,7 +7,6 @@ export const sendMessage = async (req,res) => {
         const senderId = req.id;
         const receiverId = req.params.id;
         const {textMessage:message} = req.body;
-      console.log(message);
       
         let conversation = await Conversation.findOne({
             participants:{$all:[senderId, receiverId]}
--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -3,16 +3,15 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema({
     senderId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        ref: 'User'
     },
-    receivedId: {
+    receiverId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        ref: 'User'
     },
     message: {
         type: String,
-        required: true,
-    },
-}, { timestamps: true });
-
-export const Message = mongoose.model("Message", messageSchema);
+        required: true
+    }
+});
+export const Message = mongoose.model('Message', messageSchema);
